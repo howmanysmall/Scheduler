272902e979905118b401f07c2c8eb1653b2304d7
howmanysmall <lhamil29@bruinmail.slcc.edu>
2020-02-17 14:18:23 -0700

    Update init.lua

--- src/init.lua
+++ src/init.lua
@@ -12,7 +12,7 @@ local OptionalNonNegativeNumber = t.optional(NonNegativeNumber)
 
 local DelayTuple = t.tuple(NonNegativeNumber, t.callback)
 local AddItemTuple = t.tuple(
-	t.union(t.Instance, t.table, t.RBXScriptConnection),
+	t.union(t.Instance, t.table, t.RBXScriptConnection, t.userdata),
 	OptionalNonNegativeNumber
 )
 
@@ -37,8 +37,6 @@ function Scheduler.Wait(Seconds)
 	return Seconds - TimeRemaining
 end
 
-local Scheduler_Wait = Scheduler.Wait
-
 --[[**
 	Schedules a function to be executed after DelayTime seconds have passed, without yielding the current thread. This function allows multiple Lua threads to be executed in parallel from the same stack. Unlike regular delay, this does not use the legacy scheduler and also allows passing arguments.
 
@@ -49,16 +47,46 @@ local Scheduler_Wait = Scheduler.Wait
 **--]]
 function Scheduler.Delay(DelayTime, Function, ...)
 	assert(DelayTuple(DelayTime, Function))
-	local Arguments = table.pack(...)
-	local DelayEvent = Instance.new("BindableEvent")
+	local Length = select("#", ...)
+	if Length > 0 then
+		local Arguments = {...}
+		local ExecuteTime = tick() + DelayTime
+		local Connection
 
-	DelayEvent.Event:Connect(function()
-		Scheduler_Wait(DelayTime)
-		Function(table.unpack(Arguments, 1, Arguments.n))
-		DelayEvent:Destroy()
-	end)
+		Connection = Heartbeat:Connect(function()
+			if tick() >= ExecuteTime then
+				Connection:Disconnect()
+				Function(table.unpack(Arguments, 1, Length))
+			end
+		end)
 
-	DelayEvent:Fire()
+		return Connection
+	else
+		local ExecuteTime = tick() + DelayTime
+		local Connection
+
+		Connection = Heartbeat:Connect(function()
+			if tick() >= ExecuteTime then
+				Connection:Disconnect()
+				Function()
+			end
+		end)
+
+		return Connection
+	end
+end
+
+-- @source https://devforum.roblox.com/t/psa-you-can-get-errors-and-stack-traces-from-coroutines/455510/2
+local function Call(Function, ...)
+	return Function(...)
+end
+
+local function Finish(Thread, Success, ...)
+	if not Success then
+		warn(debug.traceback(Thread, "Something went wrong! " .. tostring((...))))
+	end
+
+	return Thread, Success, ...
 end
 
 --[[**
@@ -70,46 +98,67 @@ end
 **--]]
 function Scheduler.Spawn(Function, ...)
 	assert(t.callback(Function))
-	local Arguments = table.pack(...)
-	local SpawnEvent = Instance.new("BindableEvent")
+	local Thread = coroutine.create(Call)
+	return Finish(Thread, coroutine.resume(Thread, Function, ...))
+end
 
-	SpawnEvent.Event:Connect(function()
-		Function(table.unpack(Arguments, 1, Arguments.n))
-	end)
+--[[**
+	Runs the specified function in a separate thread, without yielding the current thread. This doesn't obscure errors like spawn or coroutines do, but it does share the similar small delay that spawn has. Also allows passing arguments like coroutines do.
 
-	SpawnEvent:Fire()
-	SpawnEvent:Destroy()
+	@param [t:callback] Function The function you are executing.
+	@param [variant] ... The optional arguments you can pass that the function will execute with.
+	@returns [void]
+**--]]
+function Scheduler.SpawnDelayed(Function, ...)
+	local Length = select("#", ...)
+	if Length > 0 then
+		local Arguments = {...}
+		local Connection
+
+		Connection = Heartbeat:Connect(function()
+			Connection:Disconnect()
+			Function(table.unpack(Arguments, 1, Length))
+		end)
+	else
+		local Connection
+
+		Connection = Heartbeat:Connect(function()
+			Connection:Disconnect()
+			Function()
+		end)
+	end
 end
 
 --[[**
 	This function allows the developer to schedule the removal of the object without yielding any code. It is the suggested alternative to Debris:AddItem, as this doesn't use the legacy scheduler and also supports tables with a Destroy / destroy method.
 
-	@param [t:union<t:Instance, t:table, t:RBXScriptConnection>] Object The object to be added to destroy scheduler.
+	@param [t:union<t:Instance, t:table, t:RBXScriptConnection, t:userdata>] Object The object to be added to destroy scheduler.
 	@param [t:optional<t:numberMin<0>>] Lifetime The number of seconds before the object should be destroyed. Defaults to 10.
 	@returns [void]
 **--]]
 function Scheduler.AddItem(Object, Lifetime)
 	assert(AddItemTuple(Object, Lifetime))
-	local DebrisEvent = Instance.new("BindableEvent")
+	local ExecuteTime = tick() + (Lifetime or 10)
+	local Connection
 
-	DebrisEvent.Event:Connect(function()
-		Scheduler_Wait(Lifetime or 10)
-		if Object then
-			if Object.Destroy then
-				pcall(Object.Destroy, Object)
-			elseif Object.destroy then
-				pcall(Object.destroy, Object)
-			elseif Object.Disconnect then
-				pcall(Object.Disconnect, Object)
-			elseif Object.disconnect then
-				pcall(Object.disconnect, Object)
+	Connection = Heartbeat:Connect(function()
+		if tick() >= ExecuteTime then
+			Connection:Disconnect()
+			if Object then
+				if Object.Destroy then
+					pcall(Object.Destroy, Object)
+				elseif Object.destroy then
+					pcall(Object.destroy, Object)
+				elseif Object.Disconnect then
+					pcall(Object.Disconnect, Object)
+				elseif Object.disconnect then
+					pcall(Object.disconnect, Object)
+				end
 			end
 		end
-
-		DebrisEvent:Destroy()
 	end)
 
-	DebrisEvent:Fire()
+	return Connection
 end
 
 return Table.Lock(Scheduler)
